cb6233318b2c25684cb39e14e4366dbaed2e453f
Bartek <bartek@gmail.com>
2018-01-04 13:23:41 +0100

    Initial app setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDgQZjeIVPVIo0UxuzJf1Y6nNW5nRf_uqU",
+    authDomain: "oshop-dde30.firebaseapp.com",
+    databaseURL: "https://oshop-dde30.firebaseio.com",
+    projectId: "oshop-dde30",
+    storageBucket: "oshop-dde30.appspot.com",
+    messagingSenderId: "388063864395"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDgQZjeIVPVIo0UxuzJf1Y6nNW5nRf_uqU",
+    authDomain: "oshop-dde30.firebaseapp.com",
+    databaseURL: "https://oshop-dde30.firebaseio.com",
+    projectId: "oshop-dde30",
+    storageBucket: "oshop-dde30.appspot.com",
+    messagingSenderId: "388063864395"
+  }
 };
